da3ea03d6355bbf08c8114047a196db0e2538341
zaojue405 <101613453+zaojue405@users.noreply.github.com>
2023-05-07 13:46:13 +0800

    frist commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ReadBook
  
+
+
+这个项目是一个基于 Python 的 TXT 文件阅读器，它可以将文本文件的内容转换为语音并进行播放。在阅读过程中，用户可以随时暂停并保存进度，以便以后继续阅读。项目利用了命令行工具 edge-tts 对文本进行语音合成，并使用 mpv 播放器播放生成的音频。
+
+使用方法：
+
+确保您已经安装了 Python 3.6 或更高版本。
+使用 pip install edge-tts  安装项目所需的依赖库。
+下载项目源代码并解压。
+在终端中切换到项目目录。
+运行 python read.py 启动项目。
+根据提示输入 TXT 文件路径。
+阅读过程中，可以按 "c" 继续阅读，或按 "q" 退出。
+
+
+Project Name: Python-based TXT File Reader with Text-to-Speech (TTS)
+
+This project is a Python-based TXT file reader that converts the content of text files into speech and plays it back. Users can pause and save their progress at any time during the reading process, allowing them to continue reading later. The project uses the command-line tool edge-tts for text-to-speech synthesis and the mpv player to play the generated audio.
+
+Usage:
+
+Make sure you have Python 3.6 or higher installed.
+Install the required dependencies with pip install edge-tts .
+Download the project source code and unzip it.
+Change to the project directory in your terminal.
+Run python read.py to start the project.
+Enter the TXT file path when prompted.
+During the reading process, press "c" to continue reading or "q" to quit.
---
+++ read.py
@@ -0,0 +1,86 @@
+import os
+import subprocess
+import threading
+import queue
+
+def synthesize_audio(text, audio_queue):
+    command = f'edge-tts --voice zh-CN-XiaoyiNeural --text "{text}" --write-media book.mp3'
+    subprocess.run(command, shell=True, check=True)
+    audio_queue.put('book.mp3')
+
+
+def play_audio(audio_queue, play_lock):
+    while True:
+        audio_file = audio_queue.get()
+        if audio_file == 'STOP':
+            break
+        with play_lock:
+            command = ["mpv.exe", "-vo", "null", audio_file]
+            subprocess.run(command, check=True)
+        audio_queue.task_done()
+
+
+def read_file(file_path, start_line):
+    lines = []
+    with open(file_path, 'r', encoding='gbk') as f:
+        for i, line in enumerate(f):
+            if i >= start_line:
+                lines.append(line)
+                if len(lines) >= 100:
+                    break
+    return lines, start_line + len(lines)
+
+def save_progress(progress_file, line_number):
+    with open(progress_file, 'w') as f:
+        f.write(str(line_number))
+
+def load_progress(progress_file):
+    if os.path.exists(progress_file):
+        with open(progress_file, 'r') as f:
+            content = f.read().strip()
+            try:
+                return int(content)
+            except ValueError:
+                print(f'warning：cant load from {progress_file} ,start read from beginning。')
+                return 0
+    return 0
+
+
+
+def main():
+    file_path = input('input your TXT path：')
+    progress_file = 'progress.txt'
+    start_line = load_progress(progress_file)
+
+    audio_queue = queue.Queue()
+    play_lock = threading.Lock()
+
+    try:
+        play_thread = threading.Thread(target=play_audio, args=(audio_queue, play_lock))
+        play_thread.start()
+
+        while True:
+            lines, next_start_line = read_file(file_path, start_line)
+            if not lines:
+                print('already finished。')
+                break
+
+            for line in lines:
+                line = line.strip()
+                if len(line) > 0:
+                    synthesize_audio(line, audio_queue)
+                    with play_lock:
+                        start_line += 1
+                        save_progress(progress_file, start_line)
+
+           
+
+        audio_queue.put('STOP')
+        play_thread.join()
+    except KeyboardInterrupt:
+        print('\ndetect Ctrl+C，Saving...')
+        save_progress(progress_file, start_line)
+        print('already Saved')
+
+if __name__ == "__main__":
+    main()
