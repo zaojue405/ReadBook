687370a710bfe6969d06f414d56c3bc0bd2c2a3c
zaojue405 <101613453+zaojue405@users.noreply.github.com>
2023-05-07 23:47:52 +0800

    增加书签功能

--- read.py
+++ read.py
@@ -2,10 +2,14 @@ import os
 import subprocess
 import threading
 import queue
+import json
+"""
+voice  en-US-AriaNeural
+       zh-CN-XiaoxiaoNeural
 
-
+"""
 def synthesize_audio(text, audio_queue):
-    command = f'edge-tts --voice zh-CN-XiaoyiNeural --text "{text}" --write-media book.mp3'
+    command = f'edge-tts --voice zh-CN-XiaoxiaoNeural --text "{text}" --write-media book.mp3'
     subprocess.run(command, shell=True, check=True)
     audio_queue.put('book.mp3')
 
@@ -23,7 +27,7 @@ def play_audio(audio_queue, play_lock):
 
 def read_file(file_path, start_line):
     lines = []
-    with open(file_path, 'r', encoding='utf-8') as f:
+    with open(file_path, 'r', encoding='gbk') as f:
         for i, line in enumerate(f):
             if i >= start_line:
                 lines.append(line)
@@ -31,27 +35,31 @@ def read_file(file_path, start_line):
                     break
     return lines, start_line + len(lines)
 
-def save_progress(progress_file, line_number):
-    with open(progress_file, 'w') as f:
-        f.write(str(line_number))
+def save_progress(progress_file, book_path, current_line):
+    try:
+        with open(progress_file, 'r', encoding='utf-8') as f:
+            progress = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        progress = {}
+    progress[book_path] = current_line
+    with open(progress_file, 'w', encoding='utf-8') as f:
+        json.dump(progress, f)
 
-def load_progress(progress_file):
-    if os.path.exists(progress_file):
-        with open(progress_file, 'r') as f:
-            content = f.read().strip()
-            try:
-                return int(content)
-            except ValueError:
-                print(f'warning：cant load from {progress_file} ,start read from beginning。')
-                return 0
-    return 0
 
+def load_progress(progress_file, book_path):
+    try:
+        with open(progress_file, 'r', encoding='utf-8') as f:
+            progress = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        progress = {}
+
+    return progress.get(book_path, 0)
 
 
 def main():
-    file_path = input('input your TXT path：')
+    file_path = input('请输入TXT文件路径：')
     progress_file = 'progress.txt'
-    start_line = load_progress(progress_file)
+    start_line = load_progress(progress_file, file_path)
 
     audio_queue = queue.Queue()
     play_lock = threading.Lock()
@@ -63,7 +71,7 @@ def main():
         while True:
             lines, next_start_line = read_file(file_path, start_line)
             if not lines:
-                print('already finished。')
+                print('已经读完整个文件。')
                 break
 
             for line in lines:
@@ -72,14 +80,16 @@ def main():
                     synthesize_audio(line, audio_queue)
                     with play_lock:
                         start_line += 1
-                        save_progress(progress_file, start_line)
+                        save_progress(progress_file,  file_path,start_line)
+
+           
 
         audio_queue.put('STOP')
         play_thread.join()
     except KeyboardInterrupt:
-        print('\ndetect Ctrl+C，Saving...')
-        save_progress(progress_file, start_line)
-        print('already Saved')
+        print('\n检测到 Ctrl+C，正在保存进度...')
+        save_progress(progress_file, file_path,start_line)
+        print('进度已保存。')
     finally:
         audio_queue.put('STOP')
         play_thread.join()
